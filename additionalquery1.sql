ae52329270c239aeed12e70a066d3fbae5a41f2e
niyaojiayou <syingxi@gmail.com>
2024-04-12 16:09:06 +0800

    Update additionalquery1.sql

--- additionalquery1.sql
+++ additionalquery1.sql
@@ -1,16 +1,37 @@
 -- select the top3 mall_id with the highest number of complaints filed in the last month
-select
-    top 3
-    C.mall_id,
+select top 3
+    res.mall_id,
     count(*) as totalcomplaint
 from
-    complaint C
-WHERE
-    C.filed_date_time >= CAST(DATEADD (MONTH, -1, Getdate()) AS DATE)
-GROUP BY
-    C.mall_id
-ORDER BY
+    (                        (
+                select --- id and # complaint of the highest & lowest 3 shops
+            top 3
+            C1.shop_id,
+            count(*) as totalcomplaint
+        from
+            complaints_on_shop C1
+        WHERE
+    C1.filed_date_time >= CAST(DATEADD (MONTH, -1, Getdate()) AS DATE)
+        GROUP BY
+    C1.shop_id
+        ORDER BY
     totalcomplaint DESC
+        )
+    Union
+        (select
+            top 3
+            C2.shop_id,
+            count(*) as totalcomplaint
+        from
+            complaints_on_shop C2
+        WHERE
+    C2.filed_date_time >= CAST(DATEADD (MONTH, -1, Getdate()) AS DATE)
+        GROUP BY
+    C2.mall_id
+        ORDER BY
+    totalcomplaint)) as combined INNER JOIN shop_record ON shop.shop_id = shop_record.shop_id
+
+
 
 -- select the shop_id and the average amount spent in each shop
 SELECT
