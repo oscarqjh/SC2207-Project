1ce04cda38bccf583ffa86dad546ebdca9d1e77b
Pu Fanyi <pufanyi@gmail.com>
2024-04-17 00:37:51 +0800

    update

--- query3.sql
+++ query3.sql
@@ -4,13 +4,19 @@ GO
 -- Q3. What are the most popular recommendations from the app regarding malls?
 SELECT recommendation_id, mall_id
 FROM recommendation
-WHERE mall_id = (SELECT most_popular_mall.mall_id
-                FROM (SELECT TOP 1
-                        recommendation.mall_id, COUNT(recommendation.recommendation_id) AS total_recommendations
-                      FROM recommendation
-                      INNER JOIN user_use_recommendation 
-                      ON recommendation.recommendation_id = user_use_recommendation.recommendation_id
-                      GROUP BY recommendation.mall_id
-                      ORDER BY total_recommendations DESC) AS most_popular_mall);
+WHERE mall_id = (
+  SELECT most_popular_mall.mall_id
+  FROM (
+    SELECT TOP 1
+    recommendation.mall_id,
+    COUNT(recommendation.recommendation_id) AS total_recommendations
+    FROM recommendation
+    INNER JOIN user_use_recommendation 
+          ON recommendation.recommendation_id = user_use_recommendation.recommendation_id
+    GROUP BY recommendation.mall_id
+    ORDER BY total_recommendations DESC
+  )
+  AS most_popular_mall
+);
 
 GO
