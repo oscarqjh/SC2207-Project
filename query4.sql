a85cb5ff703447bc4aedde0ac79f748cbbf3f12d
unknown <ruochee@gmail.com>
2024-04-16 18:13:18 +0800

    Updated Query 4

--- query4.sql
+++ query4.sql
@@ -6,19 +6,9 @@ GO
 -- December 2023.
 
 GO
-SELECT X.user_account_id, X.total_spent, user_account.dob -- Show all compulsive shopppers in Dec 2023
-FROM user_account INNER JOIN
-(SELECT visits.user_account_id, SUM(visits.amount_spent) AS total_spent
-  FROM  (SELECT user_account_id, amount_spent
-        FROM shop_record
-        WHERE shop_record.date_time_in BETWEEN '2023-12-01' AND '2023-12-31'
-        ) AS visits
-  GROUP BY visits.user_account_id
-  HAVING COUNT(*) > 5) AS X
-ON user_account.user_account_id = X.user_account_id;
-
-GO
-SELECT TOP 3 X.user_account_id, X.total_spent, user_account.dob -- Show youngest 3 compulsive shoppers in Dec 2023
+-- Old query
+/*
+SELECT TOP 3 X.user_account_id, X.total_spent, user_account.dob
 FROM user_account INNER JOIN
 (SELECT visits.user_account_id, SUM(visits.amount_spent) AS total_spent
   FROM  (SELECT user_account_id, amount_spent
@@ -28,6 +18,37 @@ FROM user_account INNER JOIN
   GROUP BY visits.user_account_id
   HAVING COUNT(*) > 5) AS X
 ON user_account.user_account_id = X.user_account_id
+ORDER BY user_account.dob DESC;
+*/
+
+GO
+-- Show all shopping records and the malls visited in Dec 2023
+SELECT X.user_account_id, SUM(X.amount_spent) AS total_spent, shop.mall_id, COUNT(*) AS times_visited
+FROM shop INNER JOIN (
+  SELECT user_account_id, amount_spent, shop_id
+  FROM shop_record
+  WHERE shop_record.date_time_in BETWEEN '2023-12-01' AND '2023-12-31'
+) AS X
+ON shop.shop_id = X.shop_id
+GROUP BY X.user_account_id, shop.mall_id
+ORDER BY X.user_account_id, shop.mall_id;
+
+GO
+-- Find all compulsive shoppers in Dec 2023
+SELECT Y.user_account_id, SUM(Y.total_spent) AS total_spent, user_account.dob
+FROM user_account INNER JOIN (
+  SELECT X.user_account_id, SUM(X.amount_spent) AS total_spent, shop.mall_id
+  FROM shop INNER JOIN (
+    SELECT user_account_id, amount_spent, shop_id
+    FROM shop_record
+    WHERE shop_record.date_time_in BETWEEN '2023-12-01' AND '2023-12-31'
+  ) AS X
+  ON shop.shop_id = X.shop_id
+  GROUP BY X.user_account_id, shop.mall_id
+  HAVING COUNT(*) > 5
+) AS Y
+ON user_account.user_account_id = Y.user_account_id
+GROUP BY Y.user_account_id, user_account.dob
 ORDER BY user_account.dob DESC;
 
 GO
